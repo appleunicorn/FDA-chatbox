dde37b6dcde3230a1bb749d5e3eb0102065f51f7
appleunicorn <autumn.qiut@gmail.com>
2025-04-09 19:07:23 -0400

    minor note edits

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ This app helps you explore trends, companies, and insights in FDA first-time gen
 - **Data fields:** ANDA number, drug names, applicant/company names, approval date, ANDA indication
 - **Coverage:** Approvals from 2016 to present ( 2025 data not available on FDA website yet )
 - **Model:** DeepSeek
-- **Note:** Including approvals from acquired entities
+- **Note:** Cleaned up data includes approvals from acquired entities
             
 ---
 """)
